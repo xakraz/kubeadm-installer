c16356f2c9d58bae912025415974c01434a47eeb
Xavier Krantz <x.krantz@criteo.com>
2017-04-16 23:26:33 +0200

    [Scripts] Allow override via ENV variables in Docker image build directly

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-REPO=${REPO:-luxas/kubeadm-installer}
+K8S_VERSION=${K8S_VERSION:-v1.6.1}
 KUBEADM_VERSION=${KUBEADM_VERSION:-v1.6.0-alpha.0.2074+a092d8e0f95f52}
-KUBEADM_REVISION=${KUBEADM_REVISION:-0}
+CNI_VERSION=${CNI_VERSION:-07a8a28637e97b22eb8dfe710eeae1344f69d16e}
+
+REPO=${REPO:-luxas/kubeadm-installer}
+INSTALLER_REVISION=${KUBEADM_REVISION:-0}
 TAG_LATEST=${TAG_LATEST:-1}
 PUSH=${PUSH:-0}
 
@@ -17,13 +20,14 @@ set -u
 # https://github.com/docker/distribution/pull/1202
 KUBEADM_VERSION=${KUBEADM_VERSION/+/-g}
 
-
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
-pushd ${PROJECT_DIR}
+
 docker build \
+  -e K8S_VERSION=${K8S_VERSION} \
+  -e KUBEADM_RELEASE=${KUBEADM_VERSION} \
+  -e CNI_RELEASE=${CNI_VERSION} \
   -t ${REPO}:${KUBEADM_VERSION}.${KUBEADM_REVISION} \
-  .
-popd
+  ${PROJECT_DIR}
 
 if [[ ${TAG_LATEST} == 1 ]]; then
 	docker tag ${REPO}:${KUBEADM_VERSION}.${KUBEADM_REVISION}
