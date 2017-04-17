747336aa98efc10222d0586adae8dc09ab6b4e03
Xavier Krantz <x.krantz@criteo.com>
2017-04-17 08:35:35 +0200

    [Files] Entrypoint - Improved output messages

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -40,18 +40,24 @@ get_git_latest_release() {
 # --
 # If no values are provided, get the latest release of k8s
 if [[ -z "${K8S_VERSION}" ]]; then
+  echo "[ENV] 'K8S_VERSION' is empty -> Getting latest release version"
   K8S_VERSION=$(get_git_latest_release $K8S_REPO)
 fi
+echo "[ENV] 'K8S_VERSION' = ${K8S_VERSION}"
 
 # If no values are provided, use the same than k8s
 if [[ -z "${KUBEADM_RELEASE}" ]]; then
+  echo "[ENV] 'KUBEADM_RELEASE' is empty -> Getting same version as k8s"
   KUBEADM_RELEASE=${K8S_VERSION}
 fi
+echo "[ENV] 'KUBEADM_RELEASE' = ${KUBEADM_RELEASE}"
 
 # If no values are provided, get the latest release of CNI
 if [[ -z "${CNI_RELEASE}" ]]; then
+  echo "[ENV] 'CNI_RELEASE' is empty -> Getting latest release version"
   CNI_RELEASE=$(get_git_latest_release $CNI_REPO)
 fi
+echo "[ENV] 'CNI_RELEASE' = ${CNI_RELEASE}"
 
 
 # Kubernetes Google Cloud Storage
@@ -65,14 +71,14 @@ CNI_URL=${CNI_URL:-https://storage.googleapis.com/kubernetes-release/network-plu
 echo "Installing kubeadm version: ${KUBEADM_RELEASE}"
 if [[ -z "${KUBEADM_URL}" ]]; then
   if [[ -n "${KUBEADM_VERSION/v[0-9].[0-9].[0-9]/}" ]]; then
-    echo "Kubeadm: is a Git version"
+    echo "[Kubeadm] KUBEADM_RELEASE ${KUBEADM_RELEASE} is a Pre-release version"
     KUBEADM_URL=https://storage.googleapis.com/kubernetes-release-dev/ci-cross
   else
-    echo "Kubeadm: is a Release version"
+    echo "[Kubeadm] KUBEADM_RELEASE ${KUBEADM_RELEASE} is a Release version"
     KUBEADM_URL=https://storage.googleapis.com/kubernetes-release/release
   fi
 fi
-echo "Kubeadm: Download url - ${KUBEADM_URL}"
+echo "[Kubeadm] Download url: ${KUBEADM_URL}"
 
 
 # Linux Distro
